8324a296e7206ffb106326f2ef04bf399aa5e07c
Thomas Stringer <tr.stringer@outlook.com>
2016-07-28 17:50:04 -0400

    add logic to do all operations in single call

--- test/email-tests.js
+++ test/email-tests.js
@@ -4,6 +4,22 @@ const testConfig = require('./config.js');
 const emailUtil = require('../src/email-util');
 const fs  = require('fs');
 
+function createTestEmail(config) {
+  return new Promise((resolve, reject) => {
+    const filesystemOutboxPendingPath = config.filesystem.outboxPending;
+    const testEmailFilename = `testemail-${Math.floor(Math.random() * 100000)}.email`;
+
+    fs.writeFile(`${filesystemOutboxPendingPath}/${testEmailFilename}`, `${testConfig.testRecipient}\nthis is a email to be sent`, (err) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(config);
+      }
+    });
+  });
+}
+
 describe('configuration retrieval', () => {
   it('should retrieve config object', () => {
     return configManager.getConfiguration()
@@ -19,21 +35,9 @@ describe('configuration retrieval', () => {
 describe('emailing', () => {
   it('should send and archive emails', () => {
     return configManager.getConfiguration()
-      .then((config) => {
-        return new Promise((resolve, reject) => {
-          const filesystemOutboxPendingPath = config.filesystem.outboxPending;
-          const testEmailFilename = `testemail-${Math.floor(Math.random() * 100000)}.email`;
-          fs.writeFile(`${filesystemOutboxPendingPath}/${testEmailFilename}`, `${testConfig.testRecipient}\nthis is a email to be sent`, (err) => {
-            if (err) {
-              assert.fail(0, 1, err.message);
-              reject(err);
-            }
-            else {
-              resolve(config);
-            }
-          });
-        });
-      })
+      .then(createTestEmail)
+      .then(createTestEmail)
+      .then(createTestEmail)
       .then(emailUtil.sendAndArchive)
       .catch(assert.isUndefined);
   });
