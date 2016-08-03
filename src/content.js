164894c7c7cccf4c901d6d5a40b090e0ead26c74
Thomas Stringer <tr.stringer@outlook.com>
2016-08-03 11:31:27 -0400

    change email file name

--- src/content.js
+++ src/content.js
@@ -4,18 +4,22 @@ const fs = require('fs');
 module.exports = (() => {
   function flattenEmail(email) {
     let flattenedEmail = '';
-    flattenedEmail += `${email.created}\n`;
+    flattenedEmail += `${new Date(email.created).toString()}\n`;
     flattenedEmail += `${email.author.name} <${email.author.email}>\n\n`;
     flattenedEmail += `${email.subject}\n\n`;
     flattenedEmail += `${email.body}\n`;
 
     return flattenedEmail;
   }
+
+  function generateEmailFileName(email) {
+    return `${email.author.name.split(' ').join('-')}.${email.id.substring(email.id.lastIndexOf(':') + 1)}.email`;
+  }
   
   function saveEmailToDisk(email) {
     return new Promise((resolve, reject) => {
       const flattenedEmail = flattenEmail(email);
-      const emailFileName = `${config.inboxPath}/${email.id}.email`;
+      const emailFileName = `${config.inboxPath}/${generateEmailFileName(email)}`;
 
       fs.writeFile(emailFileName, flattenedEmail, (err) => {
         if (err) {
