eab6212d2b758cba9616021f1ad762727a57ee19
Thomas Stringer <tr.stringer@outlook.com>
2016-08-04 19:12:26 -0400

    add address book functionality

--- src/send-mail.js
+++ src/send-mail.js
@@ -3,12 +3,40 @@ const nodemailer = require('nodemailer');
 module.exports = (() => {
   function transform(emailRaw) {
     const emailLines = emailRaw.split('\n');
+    const rawRecipient = emailLines.shift();
+    const recipient = interpretRecipient(rawRecipient);
+
+    if (!recipient) {
+      throw Error(`recipient '${rawRecipient}' not found in address book`);
+    }
     
     return {
-      to: emailLines.shift(),
+      to: recipient,
       subject: emailLines.shift(),
       text: emailLines.join('\n')
     };
+  }
+
+  function interpretRecipient(recipient) {
+    // if the recipient is an email address, just return that
+    // otherwise we need to do an address book lookup
+    const emailAddressPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailAddressPattern.test(recipient) ? 
+      recipient : lookupAddressBookEmailAddress(recipient);
+  }
+
+  function lookupAddressBookEmailAddress(name) {
+    const addressBook = require(`${process.env.HOME}/.shark-mail.js`).addressBook;
+
+    const foundItems = addressBook.filter(
+      (entry) => entry.name.toLowerCase() === name.toLowerCase());
+
+    if (foundItems && foundItems.length > 0) {
+      return foundItems[0].email;
+    }
+    else {
+      return null;
+    }
   }
 
   function sendEmail(emailRaw) {
